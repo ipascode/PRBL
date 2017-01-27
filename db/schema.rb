82516bd13289aa9715634ec2372168fc7480d91f
xacasi <pasco.isabelann@gmail.com>
2017-01-27 18:11:45 +0800

    Added Links to Bus Lines, Bus Model, and changed New Bus form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125020325) do
+ActiveRecord::Schema.define(version: 20170127091326) do
 
   create_table "bus_lines", force: :cascade do |t|
     t.string   "linename"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170125020325) do
     t.float    "cpk"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "bus_no"
     t.index ["BusLine_id"], name: "index_buses_on_BusLine_id"
     t.index ["BusModel_id"], name: "index_buses_on_BusModel_id"
     t.index ["Repair_id"], name: "index_buses_on_Repair_id"
