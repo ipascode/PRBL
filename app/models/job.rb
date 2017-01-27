a5b5b73fd7ce8e6c8c552337758185ccf21ed2ec
xacasi <pasco.isabelann@gmail.com>
2017-01-27 22:00:11 +0800

    nested job parts to job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,6 @@
 class Job < ApplicationRecord
   belongs_to :mechanic
   belongs_to :repair
+  has_many :job_parts
+  accepts_nested_attributes_for :job_parts, reject_if: :all_blank, allow_destroy: true
 end
