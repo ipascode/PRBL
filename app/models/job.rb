9d72a0605560f1db0ef23378c0d401093674d558
xacasi <pasco.isabelann@gmail.com>
2017-01-28 10:30:24 +0800

    changed repair job form to simple_form

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ApplicationRecord
-  belongs_to :mechanic
-  belongs_to :repair
+  belongs_to :mechanic, foreign_key: :mechanic_id
+  belongs_to :repair, foreign_key: :repair_id
   has_many :job_parts
   accepts_nested_attributes_for :job_parts, reject_if: :all_blank, allow_destroy: true
 end
