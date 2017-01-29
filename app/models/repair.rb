95a4a28ae2e5876b85c6fb93203d741b01530c48
xacasi <pasco.isabelann@gmail.com>
2017-01-29 09:17:38 +0800

    debug: jobs now associates with repair

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -1,6 +1,6 @@
 class Repair < ApplicationRecord
-	has_many :jobs
 	belongs_to :Driver
 	belongs_to :Bus
+	has_many :jobs,  inverse_of: :repair
   	accepts_nested_attributes_for :jobs, reject_if: :all_blank, allow_destroy: true
 end
