b439f6b92928255e5b7fb390306137303010a1da
xacasi <pasco.isabelann@gmail.com>
2017-02-25 07:49:45 +0800

    added labels

--- app/assets/javascripts/repairs.js
+++ app/assets/javascripts/repairs.js
@@ -2,19 +2,7 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
-$(“.bus_no-select”).selectize(options)
 
-options = {
-    maxOptions: 5
-    render:
-        item: (item, escape) ->
-            return "<div class='bus-item'><span>#{escape(item.bus_no)}</span></div>"
-        option: (item, escape) ->
-            return "<div class='bus-item'><span>#{escape(item.bus_no)}</span></div>"
-    valueField: ‘bus_no’
-    searchField: ‘bus_no’
-    create: true
-}
 
 $('#driver-selectize').selectize({
     create: true,
