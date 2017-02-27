dd27dbe8d5bd414296d70a25d95dfa849cd8d275
xacasi <pasco.isabelann@gmail.com>
2017-02-27 13:39:48 +0800

    added close all button

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,13 @@
 $(document).ready(function() { 
+
 $('#openall').click(function(){
   $('.panel-collapse:not(".in")')
     .collapse('show');
+});
+
+$('#closeall').click(function(){
+  $('.panel-collapse.in')
+    .collapse('hide');
 });
 
 });
