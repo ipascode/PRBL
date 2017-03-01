78bd17f7d40df7ed599aaceeeb11a9cf37f0195d
xacasi <pasco.isabelann@gmail.com>
2017-03-01 16:44:04 +0800

    fixed repair delete bug

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -2,7 +2,7 @@ class Repair < ApplicationRecord
 	
 	belongs_to :driver
 	belongs_to :bus
-	has_many :jobs,  inverse_of: :repair #prevents error: jobs repair does not exist 
+	has_many :jobs,  inverse_of: :repair, dependent: :destroy #prevents error: jobs repair does not exist 
   accepts_nested_attributes_for :jobs, reject_if: :all_blank, allow_destroy: true 
 
   	scope :to_finish, lambda { where(:done => false)}
