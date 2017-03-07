c83d59ffe05dc934708f506b721b2a17b1ab29f4
xacasi <pasco.isabelann@gmail.com>
2017-03-07 21:28:47 +0800

    changes to bus.rb

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -2,11 +2,17 @@ class Bus < ApplicationRecord
   belongs_to :bus_model
   has_many :repairs
   belongs_to :bus_line
+
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
+  validates :bus_no, :presence => true
+  validates :bus_no, :plate_no, uniqueness: { case_sensitive: false }
+
+
   scope :in_repair, lambda {where(:status => "In repair")}
   
+
   require 'csv'
 
   def self.import(file)
