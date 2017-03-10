b018b88cfe8c89bea96605d21fcecc39e383df27
xacasi <pasco.isabelann@gmail.com>
2017-03-10 12:26:55 +0800

    why do the nested forms double??

--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -129,7 +129,7 @@ class RepairsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def repair_params
-      params.require(:repair).permit(:datestarted, :datefinished, :repairtype, :driver_id, :bus_id, :location, :jobcard_num, :done,
+      params.require(:repair).permit(:datestarted, :datefinished, :repairtype, :driver_id, :bus_id, :jobcard_num, :done,
         jobs_attributes: [:id, :repair_id, :mechanic_id, :timestarted, :timefinished, :jobparticular, :status, :_destroy, 
         job_parts_attributes: [:id, :part_id, :quantity, :cost, :job_id, :_destroy]])
     end
