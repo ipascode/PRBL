673171b345326baa15df48b9b732492571573b5c
xacasi <pasco.isabelann@gmail.com>
2017-03-22 10:27:57 +0800

    user import export

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :username, :email, :firstname, :lastname, :role, presence: true
+  validates :username, :email, uniqueness: {case_sensitive: false}
+
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
      user_hash = row.to_hash
-      user = User.where(lastname: user_hash["username"])
+      user = User.where(username: user_hash["username"])
 
       if user.count == 1
        user.first.update_attributes(user_hash)
@@ -16,5 +19,14 @@ class User < ApplicationRecord
       end # end if !mechanic.nil?
     end # end CSV.foreach
   end # end self.import(file)
+
+  def self.to_csv(options = {})
+  CSV.generate(options) do |csv|
+    csv << column_names
+    all.each do |product|
+      csv << product.attributes.values_at(*column_names)
+    end
+  end
+end
 
 end
