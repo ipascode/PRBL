3eb099fc7fa78221b2c8a7c2c8c3035367a3229b
xacasi <pasco.isabelann@gmail.com>
2017-03-22 17:35:14 +0800

    checkboxes + materialize js works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,7 +36,3 @@ $(document).on('ready page:load', function(){
   });
 });
 
-//$(document).ready(function() {
-// This command is used to initialize some elements and make them work properly
-//$.material.init();
-//}
