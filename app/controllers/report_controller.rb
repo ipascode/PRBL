ebe5c205c892d1014800f4464b7688b375919621
xacasi <pasco.isabelann@gmail.com>
2017-03-23 12:03:57 +0800

    filtered jobs in report

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -6,17 +6,17 @@ class ReportController < ApplicationController
 		if(params[:startDate].present? && params[:endDate].present?)
 			start_date = Time.zone.parse(params[:startDate])
 			end_date = Time.zone.parse(params[:endDate])
-			@topjobparticular = Job.datefilter(start_date, end_date.end_of_day).group(:jobparticular).group_by_month(:timefinished, format:  "%b").order("count(jobparticular) desc").limit(10).count
-			@cumulative = Job.datefilter(start_date, end_date.end_of_day).group_by_month(:timefinished).sum(:duration_minutes)
-			@avg = Job.datefilter(start_date, end_date.end_of_day).group_by_month(:timefinished).average(:duration_minutes)			
-			@jpart = Job.group(:jobparticular).average(:duration_minutes)
+			@topjobparticular = Job.done.datefilter(start_date, end_date.end_of_day).group(:jobparticular).group_by_month(:timefinished, format:  "%b").order("count(jobparticular) desc").limit(10).count
+			@cumulative = Job.done.datefilter(start_date, end_date.end_of_day).group_by_month(:timefinished).sum(:duration_minutes)
+			@avg = Job.done.datefilter(start_date, end_date.end_of_day).group_by_month(:timefinished).average(:duration_minutes)			
+			@jpart = Job.done.group(:jobparticular).average(:duration_minutes)
 			@mechanics = Mechanic.includes(:jobs).all	
 		else
-			@topjobparticular = Job.group(:jobparticular).group_by_month(:timefinished, format:  "%b").order("count(jobparticular) desc").limit(10).count
-			@cumulative = Job.group_by_month(:timefinished).sum(:duration_minutes)
-			@avg = Job.group_by_month(:timefinished).average(:duration_minutes)
-			@jpart = Job.all.group(:jobparticular).average(:duration_minutes)
-			@mechanics = Mechanic.includes(:jobs).all		
+			@topjobparticular = Job.done.group(:jobparticular).group_by_month(:timefinished, format:  "%b").order("count(jobparticular) desc").limit(10).count
+			@cumulative = Job.done.group_by_month(:timefinished).sum(:duration_minutes)
+			@avg = Job.done.group_by_month(:timefinished).average(:duration_minutes)
+			@jpart = Job.done.all.group(:jobparticular).average(:duration_minutes)
+			@mechanics = Mechanic.includes(:jobs).where('jobs.status = ? ', "Done").references(:jobs)		
 
 		end
 
