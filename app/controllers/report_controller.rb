f7261cda84c2d9a4d71338a266e8556f004bd56b
xacasi <pasco.isabelann@gmail.com>
2017-03-24 13:05:52 +0800

    some changes to charts

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -6,10 +6,16 @@ class ReportController < ApplicationController
 		if(params[:startDate].present? && params[:endDate].present?)
 			start_date = Time.zone.parse(params[:startDate])
 			end_date = Time.zone.parse(params[:endDate])
+
+			#job particular frequency
 			@topjobparticular = Job.done.datefilter(start_date, end_date.end_of_day).group(:jobparticular).group_by_month(:timefinished, format:  "%b").order("count(jobparticular) desc").limit(10).count
+			
+			#line chart repair average
 			@cumulative = Job.done.datefilter(start_date, end_date.end_of_day).group_by_month(:timefinished).sum(:duration_minutes)
 			@avg = Job.done.datefilter(start_date, end_date.end_of_day).group_by_month(:timefinished).average(:duration_minutes)			
 			@jpart = Job.done.group(:jobparticular).average(:duration_minutes)
+
+			#mechanics performance
 			@mechanics = Mechanic.includes(:jobs).where('jobs.status = ? ', "Done").references(:jobs)	
 
 			#for part cost pie chart
@@ -17,6 +23,7 @@ class ReportController < ApplicationController
 			.where('jobs.status = ? AND jobs.timefinished >= ? AND jobs.timefinished <= ?','Done', start_date, end_date.end_of_day)
 			.group(:partname).order("sum(total) desc").limit(5).sum(:total)
 
+			#parts total cost
 			@totalspentonparts = JobPart.includes(:job)
 			.where('jobs.status = ? AND jobs.timefinished >= ? AND jobs.timefinished <= ?','Done', start_date, end_date.end_of_day)
 			.sum(:total)
@@ -33,14 +40,20 @@ class ReportController < ApplicationController
 			.references(:jobs).group(:bus_model_id)
 			
 		else
+			#job particular frequency
 			@topjobparticular = Job.done.group(:jobparticular).group_by_month(:timefinished, format:  "%b").order("count(jobparticular) desc").limit(10).count
-			@cumulative = Job.done.group_by_month(:timefinished).sum(:duration_minutes)
-			@avg = Job.done.group_by_month(:timefinished).average(:duration_minutes)
+			
+			#line chart repair average
+			@avgchangeengineoil = Job.done.where("jobparticular = ?", "Change Engine Oil").group_by_day(:timefinished).average(:duration_minutes)
+			@avgchangefuelfilter = Job.done.where("jobparticular = ?", "Change Fuel Filter").group_by_day(:timefinished).average(:duration_minutes)
+			@avgchangeoilfilter = Job.done.where("jobparticular = ?", "Change Oil Filter").group_by_day(:timefinished).average(:duration_minutes)
+			
+			#mechanics performance
 			@jpart = Job.done.all.group(:jobparticular).average(:duration_minutes)
 			@mechanics = Mechanic.includes(:jobs).where('jobs.status = ? ', "Done").references(:jobs)		
 
 			#for part cost pie chart
-			@partcost = JobPart.includes(:part, :job).where('jobs.status = ? ', "Done").group(:partname).order("sum(total) desc").limit(5).sum(:total)
+			@partcost = JobPart.includes(:part, :job).where('jobs.status = ? ', "Done").group(:partname).order("sum(total) desc").limit(10).sum(:total)
 			@totalspentonparts = JobPart.includes(:job).where('jobs.status = ? ', "Done").sum(:total)
 
 			#parts cost deviation report
