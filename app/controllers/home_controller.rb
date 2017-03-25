3c5d03ff2ed27fe7603606405e17f0a9d477a6f2
xacasi <pasco.isabelann@gmail.com>
2017-03-26 00:24:44 +0800

    eager loading for home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,11 @@
 class HomeController < ApplicationController
 
   def index
-      @buses = Bus.in_repair
+      @buses = Bus.in_repair.includes(repairs: [:driver, jobs: 
+      	[:mechanics, job_parts: 
+      		[:part]]]).where('repairs.done = ?', 'False').references(:repairs)
 
   end
 
-  def exp
-      @buses = Bus.in_repair
-
-  end
   
 end
